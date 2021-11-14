c64095032424dd391d56d36571aa222bc8842c17
Omkar <omkarak45@gmail.com>
2021-11-14 14:02:20 +0530

    deploy: changed railway domain

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -29,7 +29,7 @@ async function loginRoute(req: NextApiRequest, res: NextApiResponse) {
 
 	try {
 		const response = await axios.post(
-			'https://dogemart-backend-production-b9e4.up.railway.app/auth/login',
+			'https://dogemart.up.railway.app/auth/login',
 			{
 				email,
 				password,
