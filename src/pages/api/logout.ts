284633d28ebe23faae3c3e09fefeadee04d2dcf1
Omkar <omkarak45@gmail.com>
2021-11-15 15:00:51 +0530

    fix state

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -7,5 +7,5 @@ export default withIronSessionApiRoute(logoutRoute, sessionOptions)
 
 function logoutRoute(req: NextApiRequest, res: NextApiResponse<User>) {
 	req.session.destroy()
-	res.json({ isLoggedIn: false, login: '', avatarUrl: '' })
+	res.json({ isLoggedIn: false, data: '', token: '' })
 }
